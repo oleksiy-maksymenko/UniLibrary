1d148a0824501244cd9d6dc4e8cfb13a76b17dd2
Oleksiy <maksymenko1111@gmail.com>
2021-06-27 14:26:55 +0300

    Skeleton of project creating

---
+++ src/main/java/com/book/project/border/BorderApplication.java
@@ -0,0 +1,13 @@
+package com.book.project.border;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BorderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/book/project/border/BorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.project.border;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
